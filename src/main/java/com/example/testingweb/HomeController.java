e2db3ab3857999a0adadd70870a7c4931644279e
luiscalquin <105456528+luiscalquin@users.noreply.github.com>
2024-05-10 17:57:55 -0400

    Update HomeController.java

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
 		String password="12345";
-		return "Hello, World2";
+		return "Hola, soy";
 	}
 
 }
